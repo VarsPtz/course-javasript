9e7853fd9d74f8d4e60a3034830fc6d36c3721f1
Alexandr Varugin <otz-job@mail.ru>
2018-10-23 20:06:33 +0400

    Урок №12 Основное задание.

--- lesson-12/easy/js/script.js
+++ lesson-12/easy/js/script.js
@@ -207,56 +207,125 @@ window.addEventListener("DOMContentLoaded", function () {
     //         }
     // });
 
-    function sendForm(elem) {
-        elem.addEventListener("submit", function(e) {
-            e.preventDefault();
-                elem.appendChild(statusMessage);
-                let formData = new FormData(elem);
+    // function sendForm(elem) {
+    //     elem.addEventListener("submit", function(e) {
+    //         e.preventDefault();
+    //             elem.appendChild(statusMessage);
+    //             let formData = new FormData(elem);
 
-                function postData(data) {
+    //             function postData(data) {
 
-                    return new Promise(function(resolve,reject) {
-                        let request = new XMLHttpRequest();
+    //                 return new Promise(function(resolve,reject) {
+    //                     let request = new XMLHttpRequest();
 
-                        request.open("POST", "sever.php");
+    //                     request.open("POST", "server.php");
 
-                        request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    //                     request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
-                        request.onreadystatechange = function() {
-                            if (request.readyState < 4) {
-                                resolve();
-                            } else if (request.readyState === 4) {
-                                if (request.status == 200 && request.status < 300) {
-                                    resolve();
-                                }
-                                else {
-                                    reject();
-                                }
-                            }
-                        };
-                        request.send(data);
-                    });
-                } // End postData
+    //                     request.onreadystatechange = function() {
+    //                         if (request.readyState < 4) {
+    //                             resolve();
+    //                         } else if (request.readyState === 4) {
+    //                             if (request.status == 200 && request.status < 300) {
+    //                                 resolve();
+    //                             }
+    //                             else {
+    //                                 reject();
+    //                             }
+    //                         }
+    //                     };
+    //                     request.send(data);
+    //                 });
+    //             } // End postData
                 
-                function clearInput() {
-                    for (let i = 0; i < input.length; i++) {
-                        input[i].value = "";
-                    }
-                }
+    //             function clearInput() {
+    //                 for (let i = 0; i < input.length; i++) {
+    //                     input[i].value = "";
+    //                 }
+    //             }
 
-                postData(formData)
-                    .then(() => statusMessage.style.backgroundImage = message.loading)
-                    .then(()=> {
-                        thanksModal.style.display = "block";
-                        mainModal.style.display = "none";
-                        statusMessage.style.backgroundImage = message.success;
-                    })
-                    .catch(() => statusMessage.style.backgroundImage = message.failure)
-                    .then(clearInput)
+    //             postData(formData)
+    //                 .then(() => statusMessage.style.backgroundImage = message.loading)
+    //                 .then(()=> {
+    //                     thanksModal.style.display = "block";
+    //                     mainModal.style.display = "none";
+    //                     statusMessage.style.backgroundImage = message.success;
+    //                 })
+    //                 .catch(() => statusMessage.style.backgroundImage = message.failure)
+    //                 .then(clearInput)
+    //     });
+    //     sendForm(form);
+    //     sendForm(contactForm);
+    // }
+
+
+
+
+
+    function sendForm(classForm) {
+        
+        statusMessage.classList.add('status');
+
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+            form.appendChild(statusMessage);
+
+            function postData() {
+                return new Promise(function (resolve, reject) {
+                    let request = new XMLHttpRequest(); // создаем запрос к серверу
+                    request.open('POST', 'server.php'); // выставляем настройки запроса
+                    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+                    let formData = new FormData(form); //получаем все данные с формы
+
+                    let obj = {}; // создаем объект для формата JSON
+                    for (let i = 0; i < formData.length; i++) {
+                        obj[formData[i].getAttribute('name')] = formData[i].value;
+                    }
+
+                    request.send(formData); // отправляем данные на сервер
+
+                    request.addEventListener('readystatechange', function () { // смотрим состояние запроса
+                        if (request.readyState < 4) {
+                            resolve();
+                        } else if (request.readyState == 4 && request.status == 200) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    });
+                });
+            }
+
+            function clearInput() {
+                for (let i = 0; i < input.length; i++) {
+                    input[i].value = '';
+                }
+            }
+            postData()
+                .then(() => {
+                    statusMessage.style.backgroundImage = message.loading;
+                })
+                .then(() => {
+                    statusMessage.style.backgroundImage = message.success;
+                })
+                .catch(() => {
+                    statusMessage.style.backgroundImage = message.failure;
+                })
+                .then(() => clearInput());
         });
-        sendForm(form);
-        sendForm(contactForm);
     }
+    sendForm(form);
+    sendForm(contactForm);
+    
+
+
+
+
+
+
+
+
 
     // Form contact-form
     // contactForm.addEventListener("submit", function(event) {
