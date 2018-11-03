5611c0036b8befbcc4ec6ddc59fd698ab44d93a8
Alexandr Varugin <otz-job@mail.ru>
2018-11-03 19:33:25 +0400

    Diploma - Модуль: pict пункт 9. Картинки при наведении.

--- diploma/src/js/bundle.js
+++ diploma/src/js/bundle.js
@@ -529,17 +529,91 @@ function modal() {
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return pict; });
 function pict() {
-    let imgPict =document.querySelectorAll(".main-slider-item");
-    imgPict[1].style.display = "none";
-    setInterval(() => {
-     if (imgPict[0].style.display == "none") {
-         imgPict[0].style.display = "block";
-         imgPict[1].style.display = "none";
-     } else {
-         imgPict[0].style.display = "none";
-         imgPict[1].style.display = "block";
-     }
-    }, 3000);
+    var sizeWrapper = document.querySelector('.sizes-wrapper'),
+        sizeImg1 = document.querySelector(".size-1"),
+        sizeImg2 = document.querySelector(".size-2"),
+        sizeImg3 = document.querySelector(".size-3"),
+        sizeImg4 = document.querySelector(".size-4");
+    
+    //Заменяем картинку и убираем текст
+    function showImg(e) {
+        var parImg = e.parentNode;
+        for (let i = 0; i < parImg.children.length; i++) {
+            if (parImg.children[i].classList.contains("sizes-hit")) {
+                parImg.children[i].style.display = "inline-block";
+            } else if (parImg.children[i].tagName == "P") {
+                parImg.children[i].style.display = "none";
+            }
+        }        
+        if (e.classList.contains("size-1")) {
+            e.src = "img/sizes-1-1.png";
+        } else if (e.classList.contains("size-2")) {
+            e.src = "img/sizes-2-1.png";
+        } else if (e.classList.contains("size-3")) {
+            e.src = "img/sizes-3-1.png";
+        } else if (e.classList.contains("size-4")) {
+            e.src = "img/sizes-4-1.png";
+        }
+    }
+
+    //Возвращаем подложку и текст
+    function outImg(e) {
+        var parImg = e.parentNode;
+        for (let i = 0; i < parImg.children.length; i++) {
+            if (parImg.children[i].tagName == "P") {
+                parImg.children[i].style.display = "inline";
+            }
+        }
+        if (e.classList.contains("size-1")) {
+            e.src = "img/sizes-1.png";
+        } else if (e.classList.contains("size-2")) {
+            e.src = "img/sizes-2.png";
+        } else if (e.classList.contains("size-3")) {
+            e.src = "img/sizes-3.png";
+        } else if (e.classList.contains("size-4")) {
+            e.src = "img/sizes-4.png";
+        }
+    }
+
+    //Событие - наведения мыши на блок
+    sizeWrapper.addEventListener("mouseover", function (e) {
+        var target = e.target;
+        if (target.classList.contains("sizes-hit")) {
+
+        } else {
+            showImg(target);
+        }
+    });
+
+    //Событие - мышь убирается с блока
+    sizeWrapper.addEventListener("mouseout", function (e) {
+        var target = e.target;
+        outImg(target);
+    });
+
+    //Для мобильных устройств
+    //Событие - наведения мыши на блок
+    sizeWrapper.addEventListener("touchstart", function (e) {
+        var target = e.target;
+        if (target.classList.contains("sizes-hit")) {
+
+        } else {
+            showImg(target);
+        }
+    });
+
+    //Событие - мышь убирается с блока
+    document.body.addEventListener("touchstart", function (e) {
+        var target = e.target;
+        if (target.classList.contains("sizes-block")) {
+            showImg();
+        } else {
+            sizeImg1.src = "img/sizes-1.png";
+            sizeImg2.src = "img/sizes-2.png";
+            sizeImg3.src = "img/sizes-3.png";
+            sizeImg4.src = "img/sizes-4.png";
+        }
+    });
 }
 
 /***/ }),
@@ -610,7 +684,17 @@ function portfolioTabs() {
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return slider; });
 function slider() {
-    
+    let imgPict = document.querySelectorAll(".main-slider-item");
+    imgPict[1].style.display = "none";
+    setInterval(() => {
+     if (imgPict[0].style.display == "none") {
+      imgPict[0].style.display = "block";
+      imgPict[1].style.display = "none";
+     } else {
+      imgPict[0].style.display = "none";
+      imgPict[1].style.display = "block";
+     }
+    }, 3000);
 }
 
 /***/ }),
